f1766942f93447f8d1fdda11aacbb3132723d13e
Todd Stafford <TStafford@finks.com>
2021-02-24 14:48:43 -0500

    Initial

--- Program.vb
+++ Program.vb
@@ -7,15 +7,31 @@ Imports System.Net.Mail
 Module Program
     Private dcFINKSRPT As String = "Data Source=RAYSQL;Initial Catalog=FINKS_RPT;Persist Security Info=True;User ID=sa;Password=314159"
     Sub Main()
-        DoDaily()
-        DoTeam()
-        DoCPH()
+
+        Dim dt As New DateTime
+        Dim dow As Int16
+
+        dt = Now()
+
+        dow = CInt(dt.DayOfWeek)
+
+        If dow = 0 Then
+            DoItemHistory()
+        ElseIf dow = 6 Then
+            DoHierarchyHistory()
+        End If
+
+
+        DoStoreHistory()
+        DoVendorHistory()
         DoTeamHistory()
+
+
     End Sub
 
-    Sub DoDaily()
+    Sub DoHierarchyHistory()
         Dim Conn As SqlConnection = New SqlConnection(dcFINKSRPT)
-        Dim CMD As SqlCommand = New SqlCommand("Prolecto_DailySalesDetail_Feed", Conn)
+        Dim CMD As SqlCommand = New SqlCommand("Prolecto_HIERARCH_HISTORY_Feed", Conn)
         CMD.CommandType = Data.CommandType.StoredProcedure
 
 
@@ -31,7 +47,7 @@ Module Program
         Dim FILE_NAME As String
         Dim CsvFile As New StringBuilder
 
-        FILE_NAME = "\\garnet\ProlectoSends\FINKS_DAILY_SALES_DETAIL" & ".csv"
+        FILE_NAME = "\\garnet\ProlectoSends\FINKS_HIERARCHY_HISTORY" & ".csv"
 
         Dim objWriter As New System.IO.StreamWriter(FILE_NAME)
         Try
@@ -74,13 +90,13 @@ Module Program
         Finally
 
             If Not objWriter Is Nothing Then objWriter.Close()
-            Dim DAILYreport As New Attachment(FILE_NAME.ToString)
-            SendMail(DAILYreport, (Now()), "DailyDetail")
+            Dim thereport As New Attachment(FILE_NAME.ToString)
+            SendMail(thereport, (Now()), "HIERARCHY_HISTORY")
         End Try
     End Sub
-    Sub DoTeam()
+    Sub DoStoreHistory()
         Dim Conn As SqlConnection = New SqlConnection(dcFINKSRPT)
-        Dim CMD As SqlCommand = New SqlCommand("Prolecto_DailyTeamSales_Feed", Conn)
+        Dim CMD As SqlCommand = New SqlCommand("Prolecto_STORE_HISTORY_Feed", Conn)
         CMD.CommandType = Data.CommandType.StoredProcedure
 
         Conn.Open()
@@ -96,7 +112,7 @@ Module Program
         Dim CsvFile As New StringBuilder
 
 
-        FILE_NAME = "\\garnet\ProlectoSends\FINKS_DAILY_TEAM_SALES" & ".csv"
+        FILE_NAME = "\\garnet\ProlectoSends\FINKS_STORE_HISTORY" & ".csv"
         Dim objWriter As New System.IO.StreamWriter(FILE_NAME)
         Try
 
@@ -138,14 +154,14 @@ Module Program
         Finally
 
             If Not objWriter Is Nothing Then objWriter.Close()
-            Dim TEAMreport As New Attachment(FILE_NAME.ToString)
-            SendMail(TEAMreport, (Now()), "DailyTeam")
+            Dim thereport As New Attachment(FILE_NAME.ToString)
+            SendMail(thereport, (Now()), "STORE_HISTORY")
 
         End Try
     End Sub
-    Sub DoCPH()
+    Sub DoVendorHistory()
         Dim Conn As SqlConnection = New SqlConnection(dcFINKSRPT)
-        Dim CMD As SqlCommand = New SqlCommand("Prolecto_CPH_Feed", Conn)
+        Dim CMD As SqlCommand = New SqlCommand("Prolecto_VENDOR_HISTORY_Feed", Conn)
         CMD.CommandType = Data.CommandType.StoredProcedure
 
         Conn.Open()
@@ -161,7 +177,7 @@ Module Program
         Dim CsvFile As New StringBuilder
 
 
-        FILE_NAME = "\\garnet\ProlectoSends\FINKS_CPH" & ".csv"
+        FILE_NAME = "\\garnet\ProlectoSends\FINKS_VENDOR_HISTORY" & ".csv"
         Dim objWriter As New System.IO.StreamWriter(FILE_NAME)
         Try
 
@@ -203,8 +219,73 @@ Module Program
         Finally
 
             If Not objWriter Is Nothing Then objWriter.Close()
-            Dim CPHreport As New Attachment(FILE_NAME.ToString)
-            SendMail(CPHreport, (Now()), "CPH")
+            Dim thereport As New Attachment(FILE_NAME.ToString)
+            SendMail(thereport, (Now()), "VENDOR_HISTORY")
+
+        End Try
+    End Sub
+    Sub DoItemHistory()
+        Dim Conn As SqlConnection = New SqlConnection(dcFINKSRPT)
+        Dim CMD As SqlCommand = New SqlCommand("Prolecto_ITEM_HISTORY_Feed", Conn)
+        CMD.CommandType = Data.CommandType.StoredProcedure
+
+        Conn.Open()
+
+        Dim myReader As SqlDataReader = CMD.ExecuteReader()
+        Dim dt As New Data.DataTable
+        dt.Load(myReader)
+
+
+        myReader.Close()
+
+        Dim FILE_NAME As String
+        Dim CsvFile As New StringBuilder
+
+
+        FILE_NAME = "\\garnet\ProlectoSends\FINKS_ITEM_HISTORY" & ".csv"
+        Dim objWriter As New System.IO.StreamWriter(FILE_NAME)
+        Try
+
+            ' first write a line with the columns name
+
+            Dim sep As String = ""
+
+            Dim builder As New System.Text.StringBuilder
+
+            For Each col As Data.DataColumn In dt.Columns
+
+                builder.Append(sep).Append(col.ColumnName)
+
+                sep = ","
+            Next
+
+            objWriter.WriteLine(builder.ToString())
+
+            ' then write all the rows
+
+            For Each row As Data.DataRow In dt.Rows
+
+                sep = ""
+
+                builder = New System.Text.StringBuilder
+
+                For Each col As Data.DataColumn In dt.Columns
+
+                    builder.Append(sep).Append(row(col.ColumnName))
+
+                    sep = ","
+
+                Next
+
+                objWriter.WriteLine(builder.ToString())
+
+            Next
+
+        Finally
+
+            If Not objWriter Is Nothing Then objWriter.Close()
+            Dim thereport As New Attachment(FILE_NAME.ToString)
+            SendMail(thereport, (Now()), "Item History")
 
         End Try
     End Sub
@@ -268,8 +349,8 @@ Module Program
         Finally
 
             If Not objWriter Is Nothing Then objWriter.Close()
-            Dim TeamHistoryreport As New Attachment(FILE_NAME.ToString)
-            SendMail(TeamHistoryreport, (Now()), "TeamHistory")
+            Dim thereport As New Attachment(FILE_NAME.ToString)
+            SendMail(thereport, (Now()), "Team History")
 
         End Try
     End Sub
